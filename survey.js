1e6ede387c554a565918f959c846b557d1be6567
Vinay Baswa <vinaybaswa@gmail.com>
2020-08-26 17:58:39 +0000

    asks questions takes answers and generates a profile description

--- survey.js
+++ survey.js
@@ -5,55 +5,19 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("What's your name? Nicknames are also acceptable :)", (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-  rl.question("What's an activity you like doing?", (answer) => {
-    console.log(`Thank you for your valuable feedback: ${answer}`);
-    rl.question("What's an activity you like doing?", (answer) => {
-      console.log(`Thank you for your valuable feedback: ${answer}`);
-      rl.question("What's an activity you like doing?", (answer) => {
-        console.log(`Thank you for your valuable feedback: ${answer}`);
-        rl.question("What's an activity you like doing?", (answer) => {
-          console.log(`Thank you for your valuable feedback: ${answer}`);
-          rl.question("What's an activity you like doing?", (answer) => {
-            console.log(`Thank you for your valuable feedback: ${answer}`);
-            rl.question("What's an activity you like doing?", (answer) => {
-              console.log(`Thank you for your valuable feedback: ${answer}`);
-              rl.question("What's an activity you like doing?", (answer) => {
-                console.log(`Thank you for your valuable feedback: ${answer}`);
-                rl.question("What's an activity you like doing?", (answer) => {
-                  console.log(`Thank you for your valuable feedback: ${answer}`);
-                  rl.on('resume', () => {
-                    console.log('Readline resumed.');
-                  });
-                  rl.close();
-                });
-              
-                rl.close();
-              });
-            
+rl.question("What's your name? Nicknames are also acceptable :) ", (answer1) => {
+  rl.question("What's an activity you like doing? ", (answer2) => {
+    rl.question("What do you listen to while doing that? ", (answer3) => {
+      rl.question("Which meal is your favourite (dinner, brunch,) ", (answer4) => {
+        rl.question("What's your favourite to eat for that meal? ", (answer5) => {
+          rl.question("Which sport is your absolute favourite? ", (answer6) => {
+            rl.question("What is your superpower? In a few words, ", (answer7) => {
+              console.log(`Thank you for your valuable feedback: ${answer1}\nfav activity: ${answer2}\nlistens to: ${answer3}\nfav meal: ${answer4}\nfav food: ${answer5}\nfav sport: ${answer6}\nsupower: ${answer7}\n`);
               rl.close();
             });
-          
-            rl.close();
           });
-        
-          rl.close();
         });
-      
-        rl.close();
       });
-    
-      rl.close();
     });
-  
-    rl.close();
   });
-  rl.close();
-});
-
-rl.question("What's an activity you like doing?", (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
 });
